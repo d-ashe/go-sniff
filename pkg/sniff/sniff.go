8a8868d20ac94b3a2a4ad83a0baf1bae874434ea
Duncan Ashe <d.ashe@cointel.ai>
2021-06-11 12:02:51 -0500

    bulk insert success

--- pkg/sniff/sniff.go
+++ pkg/sniff/sniff.go
@@ -1,33 +1,112 @@
 package sniff
 
 import (
-  "fmt"
+  "time"
+  "context"
+  "sync"
+  "bytes"
+  "encoding/json"
 
 	"github.com/sirupsen/logrus"
 	"github.com/google/gopacket"
   "github.com/google/gopacket/layers"
   "github.com/google/gopacket/pfring"
 	"github.com/elastic/go-elasticsearch/v8"
+  "github.com/elastic/go-elasticsearch/v8/esutil"
 )
 
-func handlePacket(packet gopacket.Packet) {
-  fmt.Println(packet)
-  logrus.Debug("Packet handled")
+type PacketDocument struct {
+  SrcMAC   string   `json:"src_mac"`
+  DstMAC   string   `json:"dst_mac"`
+  SrcIP    string   `json:"src_ip"`
+  DstIP    string   `json:"dst_ip"`
+  SrcPort  string   `json:"src_port"`
+  DstPort  string   `json:"dst_port"`
+}
+
+func insertPackets(packetsIn chan gopacket.Packet, done chan struct{}, wg *sync.WaitGroup) {
+  defer wg.Done()
+  es, err := elasticsearch.NewDefaultClient()
+  if err != nil {
+    logrus.Fatal("Error creating the client: %s", err)
+    panic(err)
+  }
+  res, err := es.Info()
+  if err != nil {
+    logrus.Fatal("Error getting response: %s", err)
+    panic(err)
+  }
+  logrus.Debug(res)
+  bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+    Index:         "packet",        // The default index name
+    Client:        es,               // The Elasticsearch client
+    NumWorkers:    20,       // The number of worker goroutines
+    FlushBytes:    int(5e+6),  // The flush threshold in bytes
+    FlushInterval: 30 * time.Second, // The periodic flush interval
+  })
+  if err != nil {
+    logrus.Fatal("Error creating the indexer: %s", err)
+    panic(err)
+  }
+  for {
+		select {
+		case <-done:
+			return
+    case data := <-packetsIn:
+      bytesData, err := json.Marshal(data)
+		  if err != nil {
+		  	logrus.Fatal("Cannot encode article %d: %s", err)
+		  }
+      err = bi.Add(
+		  	context.Background(),
+		  	esutil.BulkIndexerItem{
+		  		// Action field configures the operation to perform (index, create, delete, update)
+		  		Action: "index",
+  
+		  		// Body is an `io.Reader` with the payload
+		  		Body: bytes.NewReader(bytesData),
+  
+		  		// OnSuccess is called for each successful operation
+		  		OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem) {
+		  			logrus.Debug("Bulk insert success")
+		  		},
+  
+		  		// OnFailure is called for each failed operation
+		  		OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
+		  			if err != nil {
+		  				logrus.Fatal("ERROR: %s", err)
+		  			} else {
+		  				logrus.Fatal("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
+		  			}
+		  		},
+		  	},
+		  )
+		if err != nil {
+			logrus.Fatal("Unexpected error: %s", err)
+		}
+  }
+}
 }
 
 func iterPackets(packetSource *gopacket.PacketSource) {
-  es, err := elasticsearch.NewDefaultClient()
-    if err != nil {
-      logrus.Fatal("Error creating the client: %s", err)
-    }
-    res, err := es.Info()
-    if err != nil {
-      logrus.Fatal("Error getting response: %s", err)
-    }
-    logrus.Debug(res)
+  var wg sync.WaitGroup
+	packetsOut := make(chan gopacket.Packet)
+	done := make(chan struct{})
+
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
     for packet := range packetSource.Packets() {
-      handlePacket(packet)  // Do something with a packet here.
+      packetsOut <- packet
     }
+  }()
+
+  wg.Add(1)
+  insertPackets(packetsOut, done, &wg)
+  //for packet := range packetSource.Packets() {
+    //handlePacket(packet)  // Do something with a packet here.
+    //packetsOut = append(packetsOut, &packet)
+  //}
 }
 
 //Sniff takes ifname and filter string as parameter. 
